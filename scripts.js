a178991d9ce060620e56bf1b5a6385c0a9b2010b
mlnitoon2 <77251330+mlnitoon2@users.noreply.github.com>
2021-03-13 14:40:42 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const dino = document.getElementById("dino")
+const cactus = document.getElementById("cactus")
+
+
+
+
+function jump() {
+    if (dino.classList != "jump"){
+        dino.classList.add("jump");
+        setTimeout(function () {
+            dino.classList.remove("jump")
+           }, 300)
+    }
+}
+
+let isAlive = setInterval(function () {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusleft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+    if (cactusleft <50 && cactusleft > 0 && dinoTop >= 140){
+        console.log("collision is happening")
+        alert("Game Over!")
+    }
+}, 10)
+
+document.addEventListener("keydown", function (event) {
+  jump();
+
+});
